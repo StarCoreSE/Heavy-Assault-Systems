bcbbba4c92f9a7c70c34cf1f68646576dc605118
InvalidArgument3 <51190031+InvalidArgument3@users.noreply.github.com>
2023-04-10 23:53:21 -0500

    init commit

---
+++ Data/Scripts/CoreParts/MasterConfig.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Scripts
+{
+    partial class Parts
+    {
+        internal Parts()
+        {
+            // naming convention: WeaponDefinition Name
+            //SA_HMI_ERGaussRF
+            // Enable your definitions using the follow syntax:
+            // PartDefinitions(Your1stDefinition, Your2ndDefinition, Your3rdDefinition);
+            // PartDefinitions includes both weapons and phantoms
+            PartDefinitions(
+            HAS_Assault,
+            HAS_Thanatos,
+            HAS_Alecto,
+            HAS_Mammon,
+            HAS_Nott,
+            HAS_Hecate,
+            HAS_Twin,
+            HAS_Esper,
+            HAS_Vulcan
+            );
+            ArmorDefinitions();
+            SupportDefinitions();
+            UpgradeDefinitions();
+        }
+    }
+}
