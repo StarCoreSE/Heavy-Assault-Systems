b1151593ce9c6af8d620152b419490d9365da0af
Phantomewzick <Phantomewzick@gmail.com>
2023-05-14 18:50:33 -0500

    Crossfield Tweaks

--- Data/Scripts/CoreParts/CrossfieldThree.cs
+++ Data/Scripts/CoreParts/CrossfieldThree.cs
@@ -82,8 +82,8 @@ namespace Scripts {
                 },
                 HardWare = new HardwareDef
                 {
-                    RotateRate = 0.01f, // Max traversal speed of azimuth subpart in radians per tick (0.1 is approximately 360 degrees per second).
-                    ElevateRate = 0.01f, // Max traversal speed of elevation subpart in radians per tick.
+                    RotateRate = 0.02f, // Max traversal speed of azimuth subpart in radians per tick (0.1 is approximately 360 degrees per second).
+                    ElevateRate = 0.02f, // Max traversal speed of elevation subpart in radians per tick.
                     MinAzimuth = -45,
                     MaxAzimuth = 45,
                     MinElevation = -5,
