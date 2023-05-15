c89627f9c3b15f1c32ce0b66699ad651a00b0712
Phantomewzick <Phantomewzick@gmail.com>
2023-05-14 19:01:38 -0500

    Crossfield Tracking and Traverse Tweaks

--- Data/Scripts/CoreParts/CrossfieldThree.cs
+++ Data/Scripts/CoreParts/CrossfieldThree.cs
@@ -58,7 +58,7 @@ namespace Scripts {
                 PartName = "Crossfield", // Name of the weapon in terminal, should be unique for each weapon definition that shares a SubtypeId (i.e. multiweapons).
                 DeviateShotAngle = 0f, // Projectile inaccuracy in degrees.
                 AimingTolerance = 45f, // How many degrees off target a turret can fire at. 0 - 180 firing angle.
-                AimLeadingPrediction = Off, // Level of turret aim prediction; Off, Basic, Accurate, Advanced
+                AimLeadingPrediction = Basic, // Level of turret aim prediction; Off, Basic, Accurate, Advanced
                 DelayCeaseFire = 0, // Measured in game ticks (6 = 100ms, 60 = 1 second, etc..). Length of time the weapon continues firing after trigger is released.
                 AddToleranceToTracking = true, // Allows turret to track to the edge of the AimingTolerance cone instead of dead centre.
                 CanShootSubmerged = false, // Whether the weapon can be fired underwater when using WaterMod.
