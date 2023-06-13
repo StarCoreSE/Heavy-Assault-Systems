5537c9accb2e2932e41cbb0b180bf29673551bc4
Phantomewzick <Phantomewzick@gmail.com>
2023-06-12 20:02:06 -0500

    Crossfield Ammo reference fixed, now has correct ammo amount.

--- Data/Scripts/CoreParts/Cyclops Ammo.cs
+++ Data/Scripts/CoreParts/Cyclops Ammo.cs
@@ -32,7 +32,7 @@ namespace Scripts
     {
         private AmmoDef HAS_Cyclops_Ammo => new AmmoDef // Your ID, for slotting into the Weapon CS
         {
-            AmmoMagazine = "CrossfieldAmmoMag", // SubtypeId of physical ammo magazine. Use "Energy" for weapons without physical ammo.
+            AmmoMagazine = "CyclopsAmmoMag", // SubtypeId of physical ammo magazine. Use "Energy" for weapons without physical ammo.
             AmmoRound = "Cyclops Beam", // Name of ammo in terminal, should be different for each ammo type used by the same weapon. Is used by Shrapnel.
             HybridRound = true, // Use both a physical ammo magazine and energy per shot.
             EnergyCost = 0.175f, // Scaler for energy per shot (EnergyCost * BaseDamage * (RateOfFire / 3600) * BarrelsPerShot * TrajectilesPerBarrel). Uses EffectStrength instead of BaseDamage if EWAR.
@@ -42,7 +42,7 @@ namespace Scripts
             BackKickForce = 0f, // Recoil. This is applied to the Parent Grid.
             DecayPerShot = 0f, // Damage to the firing weapon itself.
             HardPointUsable = true, // Whether this is a primary ammo type fired directly by the turret. Set to false if this is a shrapnel ammoType and you don't want the turret to be able to select it directly.
-            EnergyMagazineSize = 120, // For energy weapons, how many shots to fire before reloading.
+            EnergyMagazineSize = 600, // For energy weapons, how many shots to fire before reloading.
             IgnoreWater = false, // Whether the projectile should be able to penetrate water when using WaterMod.
             IgnoreVoxels = false, // Whether the projectile should be able to penetrate voxels.
             Synchronize = false, // For future use
